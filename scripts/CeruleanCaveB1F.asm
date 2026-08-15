31f3de5d39b2390cce0ae183e1214dbca8cbb22b
knabepicer <anishry00@gmail.com>
2026-08-14 21:46:18 -0500

    added dialogue after mewtwo to start mew quest

--- scripts/CeruleanCaveB1F.asm
+++ scripts/CeruleanCaveB1F.asm
@@ -9,20 +9,34 @@ CeruleanCaveB1F_Script:
 
 CeruleanCaveB1F_ScriptPointers:
 	def_script_pointers
-	dw_const CheckFightingMapTrainers,              SCRIPT_CERULEANCAVEB1F_DEFAULT
+	dw_const CeruleanCaveDefaultScript,              SCRIPT_CERULEANCAVEB1F_DEFAULT
 	dw_const DisplayEnemyTrainerTextAndStartBattle, SCRIPT_CERULEANCAVEB1F_START_BATTLE
 	dw_const EndTrainerBattle,                      SCRIPT_CERULEANCAVEB1F_END_BATTLE
+
+CeruleanCaveDefaultScript:
+	CheckEventHL EVENT_BEAT_MEWTWO
+	jp nz, CheckFightingMapTrainers
+	CheckEventReuseHL EVENT_FIGHT_MEWTWO
+	ResetEventReuseHL EVENT_FIGHT_MEWTWO
+	jp z, CheckFightingMapTrainers
+	SetEvent EVENT_BEAT_MEWTWO
+	ld a, TEXT_CERULEANCAVEB1F_MEWTWO_POST
+	ldh [hTextID], a
+	call DisplayTextID
+	call Delay3
+	ret
 
 CeruleanCaveB1F_TextPointers:
 	def_text_pointers
 	dw_const CeruleanCaveB1FMewtwoText, TEXT_CERULEANCAVEB1F_MEWTWO
 	dw_const PickUpItemText,            TEXT_CERULEANCAVEB1F_ULTRA_BALL
 	dw_const PickUpItemText,            TEXT_CERULEANCAVEB1F_MAX_REVIVE
+	dw_const CeruleanCaveB1FMewtwoPostText,		TEXT_CERULEANCAVEB1F_MEWTWO_POST
 
 CeruleanCaveB1FTrainerHeaders:
 	def_trainers
 MewtwoTrainerHeader:
-	trainer EVENT_BEAT_MEWTWO, 0, MewtwoBattleText, MewtwoBattleText, MewtwoBattleText
+	trainer EVENT_FIGHT_MEWTWO, 0, MewtwoBattleText, MewtwoBattleText, MewtwoBattleText
 	db -1 ; end
 
 CeruleanCaveB1FMewtwoText:
@@ -38,3 +52,7 @@ MewtwoBattleText:
 	call PlayCry
 	call WaitForSoundToFinish
 	jp TextScriptEnd
+
+CeruleanCaveB1FMewtwoPostText:
+	text_far _CeruleanCaveB1FMewtwoPostText
+	text_end
--- text/CeruleanCaveB1F.asm
+++ text/CeruleanCaveB1F.asm
@@ -1,3 +1,14 @@
 _MewtwoBattleText::
 	text "Mew!@"
 	text_end
+
+_CeruleanCaveB1FMewtwoPostText::
+	text "As the battle"
+	line "ends, you see a"
+	cont "figure vanishing"
+	cont "into the shadows,"
+	cont "across the water."
+
+	cont "Was that..."
+	cont "...MR. FUJI!?"
+	done
